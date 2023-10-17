0240ff649d7c897c635d8cd87a8d1853d41d91c5
Will Campbell <wpcampbell1@gmail.com>
2023-10-17 16:44:47 -0400

    added new news api

--- react-app/src/components/Trending/NewsComponent.js
+++ react-app/src/components/Trending/NewsComponent.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { thunkGetNews } from '../../store/news';
 import './NewsComponent.css';
 
 const NewsComponent = () => {
-    const newsData = useSelector(state => state.news.news.articles);
+    const newsData = useSelector(state => state.news.news.data);
     const dispatch = useDispatch();
-    console.log("newsData", newsData);
 
     useEffect(() => {
         dispatch(thunkGetNews());
     }, []);
 
+    // Check if newsData is defined and not an empty array before accessing it
+    const newsItems = newsData && newsData.length > 0 ? newsData[0]?.screen_data.analysis : [];
 
     return (
         <div className="news-card">
-            <div id='newsCardTitle'>News:</div>
+            <div id='newsCardTitle'>Analysis:</div>
             <ul>
-                {newsData?.map((newsItem, index) => (
+                {newsItems.map((newsItem, index) => (
                     <li key={index} className="news-item">
                         <div className="news-image">
-                            <img src={newsItem.urlToImage} alt="news" />
+                            <img src={newsItem.related_image} alt="news" />
                         </div>
                         <div className="news-content">
-                            <a href={newsItem.url} className="news-title">
-                                {newsItem.title}
+                            <a href={newsItem.article_href} className="news-title">
+                                {newsItem.article_title}
                             </a>
-                            <p className="news-description">{newsItem.description}</p>
+                            {/* <p className="news-description">{newsItem.description}</p> */}
                         </div>
                     </li>
                 ))}
--- react-app/src/store/news.js
+++ react-app/src/store/news.js
@@ -13,11 +13,33 @@ const getNews = (news) => ({
 //THUNK:
 
 export const thunkGetNews = () => async (dispatch) => {
+
+
+    const url = 'https://investing-cryptocurrency-markets.p.rapidapi.com/coins/get-analysis?pair_ID=1057391&time_utc_offset=28800&lang_ID=1&page=1';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '91640f8edcmsh36e736e8e77134cp161807jsnbd83bf2cfb69',
+            'X-RapidAPI-Host': 'investing-cryptocurrency-markets.p.rapidapi.com'
+        }
+    };
+    
+    // try {
+    //     const response = await fetch(url, options);
+    //     const result = await response.text();
+    //     console.log(result);
+    // } catch (error) {
+    //     console.error(error);
+    // }
+
+
+
     const currentDate = new Date();
     const oneWeekAgo = new Date(currentDate);
     oneWeekAgo.setDate(currentDate.getDate() - 7);
     const formattedDate = oneWeekAgo.toISOString().split('T')[0];
-    const response = await fetch(`https://newsapi.org/v2/everything?q=cryptocurrency&pageSize=20&from=${formattedDate}&apiKey=1f6448a567ea4d85b93ba930044a6250`);
+    // const response = await fetch(`https://newsapi.org/v2/everything?q=cryptocurrency&pageSize=20&from=${formattedDate}&apiKey=1f6448a567ea4d85b93ba930044a6250`);
+    const response = await fetch(url, options)
     if (response.ok) {
         const news = await response.json();
         dispatch(getNews(news));
